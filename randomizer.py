3bff25b7c83f401dd4f6ad3e52635879f0f3dd8b
grantp5 <105947011+grantp5@users.noreply.github.com>
2022-05-21 17:27:39 +0100

    Add files via upload

--- randomizer.py
+++ randomizer.py
@@ -1,135 +1,78 @@
 import random
 
-def shuffle_lit_tu(lit):
-    return random.sample(lit, 4)
-def shuffle_hist_tu(hist):
-    return random.sample(hist, 4)
-def shuffle_sci_tu(sci):
-    return random.sample(sci, 4)
-def shuffle_fa_tu(fa):
-    return random.sample(fa, 4)
-def shuffle_rmpss_tu(rmpss):
-    return random.sample(rmpss, 4)
+def shuffle(lit, hist, sci, fa, rmpss):
+    litT = random.sample(lit, 4)
+    litB = generateDerangement(litT)
 
-def shuffle_lit_b(lit):
-    return random.sample(lit, 4)
-def shuffle_hist_b(hist):
-    return random.sample(hist, 4)
-def shuffle_sci_b(sci):
-    return random.sample(sci, 4)
-def shuffle_fa_b(fa):
-    return random.sample(fa, 4)
-def shuffle_rmpss_b(rmpss):
-    return random.sample(rmpss, 4)
+    histT = random.sample(hist, 4)
+    histB = generateDerangement(histT)
 
-def fill_quarters_tu(shuffled_lit_tu, shuffled_hist_tu, shuffled_sci_tu, shuffled_fa_tu, shuffled_rmpss_tu):
-    first_quarter_tu = list([shuffled_lit_tu[0], shuffled_hist_tu[0], shuffled_sci_tu[0], shuffled_fa_tu[0], shuffled_rmpss_tu[0]])
-    second_quarter_tu = list([shuffled_lit_tu[1], shuffled_hist_tu[1], shuffled_sci_tu[1], shuffled_fa_tu[1], shuffled_rmpss_tu[1]])
-    third_quarter_tu = list([shuffled_lit_tu[2], shuffled_hist_tu[2], shuffled_sci_tu[2], shuffled_fa_tu[2], shuffled_rmpss_tu[2]])
-    fourth_quarter_tu = list([shuffled_lit_tu[3], shuffled_hist_tu[3], shuffled_sci_tu[3], shuffled_fa_tu[3], shuffled_rmpss_tu[3]])
-    return random.sample(first_quarter_tu, 5) + random.sample(second_quarter_tu, 5) + random.sample(third_quarter_tu, 5) + random.sample(fourth_quarter_tu, 5)
-def fill_quarters_b(shuffled_lit_b, shuffled_hist_b, shuffled_sci_b, shuffled_fa_b, shuffled_rmpss_b):
-    first_quarter_b = list([shuffled_lit_b[0], shuffled_hist_b[0], shuffled_sci_b[0], shuffled_fa_b[0], shuffled_rmpss_b[0]])
-    second_quarter_b = list([shuffled_lit_b[1], shuffled_hist_b[1], shuffled_sci_b[1], shuffled_fa_b[1], shuffled_rmpss_b[1]])
-    third_quarter_b = list([shuffled_lit_b[2], shuffled_hist_b[2], shuffled_sci_b[2], shuffled_fa_b[2], shuffled_rmpss_b[2]])
-    fourth_quarter_b = list([shuffled_lit_b[3], shuffled_hist_b[3], shuffled_sci_b[3], shuffled_fa_b[3], shuffled_rmpss_b[3]])
-    return random.sample(first_quarter_b, 5) + random.sample(second_quarter_b, 5) + random.sample(third_quarter_b, 5) + random.sample(fourth_quarter_b, 5)
+    sciT = random.sample(sci, 4)
+    sciB = generateDerangement(sciT)
 
-def check_pairs(final_order_tu, final_order_b):
-    condition = 0
-    for x in range(len(final_order_tu)):
-        if "Literature" in final_order_tu[x] and "Literature" in final_order_b[x]:
-            condition += 1
-        elif "History" in final_order_tu[x] and "History" in final_order_b[x]:
-            condition += 1
-        elif "Science" in final_order_tu[x] and "Science" in final_order_b[x]:
-            condition += 1
-        elif "Fine Arts" in final_order_tu[x] and "Fine Arts" in final_order_b[x]:
-            condition += 1
-        elif "Beliefs" in final_order_tu[x] and "Beliefs" in final_order_b[x]:
-            condition += 1
-        elif "Thought" in final_order_tu[x] and "Thought" in final_order_b[x]:
-            condition += 1
-    if condition == 0:
-        return False
-    else:
-        return True
+    faT = random.sample(fa, 4)
+    faB = generateDerangement(faT)
 
-def check_adjacent_categories(final_order_tu, final_order_b):
-    condition = 0
-    for x in range(len(final_order_tu)-1):
-        if "Literature" in final_order_tu[x] and "Literature" in final_order_tu[x+1]:
-            condition += 1
-        elif "History" in final_order_tu[x] and "History" in final_order_tu[x+1]:
-            condition += 1
-        elif "Science" in final_order_tu[x] and "Science" in final_order_tu[x+1]:
-            condition += 1
-        elif "Fine Arts" in final_order_tu[x] and "Fine Arts" in final_order_tu[x+1]:
-            condition += 1
-        elif "Beliefs" in final_order_tu[x] and "Beliefs" in final_order_tu[x+1]:
-            condition += 1
-        elif "Thought" in final_order_tu[x] and "Thought" in final_order_tu[x+1]:
-            condition += 1
-        elif "Literature" in final_order_b[x] and "Literature" in final_order_b[x+1]:
-            condition += 1
-        elif "History" in final_order_b[x] and "History" in final_order_b[x+1]:
-            condition += 1
-        elif "Science" in final_order_b[x] and "Science" in final_order_b[x+1]:
-            condition += 1
-        elif "Fine Arts" in final_order_b[x] and "Fine Arts" in final_order_b[x+1]:
-            condition += 1
-        elif "Beliefs" in final_order_b[x] and "Beliefs" in final_order_b[x+1]:
-            condition += 1
-        elif "Thought" in final_order_b[x] and "Thought" in final_order_b[x+1]:
-            condition += 1
-    if condition == 0:
-        return False
-    else:
-        return True
+    rmpssT = random.sample(rmpss, 4)
+    rmpssB = generateDerangement(rmpssT)
 
-def check_adjacent_b_tu(final_order_tu, final_order_b):
-    condition = 0
-    for x in range(len(final_order_tu)-1):
-        if "Literature" in final_order_b[x] and "Literature" in final_order_tu[x+1]:
-            condition += 1
-        elif "History" in final_order_b[x] and "History" in final_order_tu[x+1]:
-            condition += 1
-        elif "Science" in final_order_b[x] and "Science" in final_order_tu[x+1]:
-            condition += 1
-        elif "Fine Arts" in final_order_b[x] and "Fine Arts" in final_order_tu[x+1]:
-            condition += 1
-        elif "Beliefs" in final_order_b[x] and "Beliefs" in final_order_tu[x+1]:
-            condition += 1
-        elif "Thought" in final_order_b[x] and "Thought" in final_order_tu[x+1]:
-            condition += 1
-    if condition == 0:
-        return False
-    else:
-        return True
+    tossups = {"lit": litT, "hist": histT, "sci": sciT, "fa": faT, "rmpss": rmpssT}
+    bonuses = {"lit": litB, "hist": histB, "sci": sciB, "fa": faB, "rmpss": rmpssB}
+
+    return tossups, bonuses
+
+def generateDerangement(cats):
+    derangements = ["1230", "2031", "3012", "1302", "2301", "3201", "1032", "2310", "3012"]
+    d = [int(x) for x in random.choice(derangements)]
+    return [cats[d[0]], cats[d[1]], cats[d[2]], cats[d[3]]]
+
+def generateOrder(tossups, bonuses):
+    q1t = [tossups["lit"][0], tossups["hist"][0], tossups["sci"][0], tossups["fa"][0], tossups["rmpss"][0]]
+    q2t = [tossups["lit"][1], tossups["hist"][1], tossups["sci"][1], tossups["fa"][1], tossups["rmpss"][1]]
+    q3t = [tossups["lit"][2], tossups["hist"][2], tossups["sci"][2], tossups["fa"][2], tossups["rmpss"][2]]
+    q4t = [tossups["lit"][3], tossups["hist"][3], tossups["sci"][3], tossups["fa"][3], tossups["rmpss"][3]]
+
+    q1b = [bonuses["lit"][0], bonuses["hist"][0], bonuses["sci"][0], bonuses["fa"][0], bonuses["rmpss"][0]]
+    q2b = [bonuses["lit"][1], bonuses["hist"][1], bonuses["sci"][1], bonuses["fa"][1], bonuses["rmpss"][1]]
+    q3b = [bonuses["lit"][2], bonuses["hist"][2], bonuses["sci"][2], bonuses["fa"][2], bonuses["rmpss"][2]]
+    q4b = [bonuses["lit"][3], bonuses["hist"][3], bonuses["sci"][3], bonuses["fa"][3], bonuses["rmpss"][3]]
+
+    tossupOrder = random.sample(q1t, 5) + random.sample(q2t, 5) + random.sample(q3t, 5) + random.sample(q4t, 5)
+    bonusOrder = random.sample(q1b, 5) + random.sample(q2b, 5) + random.sample(q3b, 5) + random.sample(q4b, 5)
+
+    return tossupOrder, bonusOrder
+
+def check_adjacent_categories(final_order_tu, final_order_b, categories):
+    for i in range(len(final_order_tu) - 1):
+        if categories[final_order_tu[i]] == categories[final_order_tu[i+1]]:
+            return True
+        elif categories[final_order_b[i]] == categories[final_order_b[i+1]]:
+            return True
+        if categories[final_order_b[i]] == categories[final_order_tu[i+1]]:
+            return True
+        if categories[final_order_tu[i]] == categories[final_order_b[i]]:
+            return True
+    return False
 
 def check_same_quarter_repeats(final_order_tu, final_order_b):
-    condition = 0
-    for x in range(len(final_order_tu)-15):
-        tu = final_order_tu[x]
+    for i in range(len(final_order_tu)-15):
+        tu = final_order_tu[i]
         b_position = final_order_b.index(tu)
         if b_position <= 4:
-            condition += 1
-    for x in range(len(final_order_tu)-15):
-        tu = final_order_tu[x+5]
+            return True
+    for i in range(len(final_order_tu)-15):
+        tu = final_order_tu[i+5]
         b_position = final_order_b.index(tu)
         if b_position >= 5 and b_position <=9:
-            condition += 1
-    for x in range(len(final_order_tu)-15):
-        tu = final_order_tu[x+10]
+            return True
+    for i in range(len(final_order_tu)-15):
+        tu = final_order_tu[i+10]
         b_position = final_order_b.index(tu)
         if b_position >= 10 and b_position <= 14:
-            condition += 1
-    for x in range(len(final_order_tu)-15):
-        tu = final_order_tu[x+15]
+            return True
+    for i in range(len(final_order_tu)-15):
+        tu = final_order_tu[i+15]
         b_position = final_order_b.index(tu)
         if b_position >= 15:
-            condition += 1
-    if condition == 0:
-        return False
-    else:
-        return True
+            return True
+    return False
